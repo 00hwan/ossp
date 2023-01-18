b6947f6e5282562ec5afb66147cf282037c774db
00hwan <122016154+00hwan@users.noreply.github.com>
2023-01-19 00:04:18 +0900

    Update minesweeper.py
    기존 코드 분석한 파일

--- minesweeper.py
+++ minesweeper.py
@@ -9,11 +9,11 @@ import platform
 import time
 from datetime import time, date, datetime
 
-#지뢰찾기 창의 크기
+#지뢰찾기 창의 크기 10X10으로 고정
 SIZE_X = 10
 SIZE_Y = 10
 
-#뭔지 아직 모르겠음
+#칸의 상태에 따라 변수 설정
 STATE_DEFAULT = 0
 STATE_CLICKED = 1
 STATE_FLAGGED = 2
@@ -24,9 +24,9 @@ BTN_FLAG = "<Button-2>" if platform.system() == 'Darwin' else "<Button-3>"
 
 window = None
 
-#
 class Minesweeper:
 
+    #초기 지뢰 창 UI
     def __init__(self, tk):
 
         # import images
@@ -46,28 +46,33 @@ class Minesweeper:
         self.frame = Frame(self.tk)
         self.frame.pack()
 
-        # set up labels/UI =>지뢰찾기 창 UI 시간, 지뢰 갯수, 깃발
+        # set up labels/UI => 지뢰찾기 창 UI 시간, 지뢰 갯수, 깃발 초기
         self.labels = {
             "time": Label(self.frame, text = "00:00:00"),
             "mines": Label(self.frame, text = "Mines: 0"),
             "flags": Label(self.frame, text = "Flags: 0")
         }
         #GUI 작업
+        #시간을 상단 가운데에 위치시킴
+        #지뢰의 수를 10*10 창의 크기 바로 밑 좌측에 위치시킴
+        #찾은 지뢰의 수를 10*10 창의 크기 바로 밑 11번째 행 우측에 위치시킴
         self.labels["time"].grid(row = 0, column = 0, columnspan = SIZE_Y) # top full width 
         self.labels["mines"].grid(row = SIZE_X+1, column = 0, columnspan = int(SIZE_Y/2)) # bottom left
         self.labels["flags"].grid(row = SIZE_X+1, column = int(SIZE_Y/2)-1, columnspan = int(SIZE_Y/2)) # bottom right
+        self.restart() # start game 게임시작 함수호출
+        self.updateTimer() # init timer 타이머 초기
 
-        self.restart() # start game
-        self.updateTimer() # init timer
-
+    #
     def setup(self):
         # create flag and clicked tile variables
+        #변수 초기
         self.flagCount = 0
         self.correctFlagCount = 0
         self.clickedCount = 0
         self.startTime = None
 
         # create buttons
+        # 10*10 크기의 지뢰 map을 만들기 위함
         self.tiles = dict({})
         self.mines = 0
         for x in range(0, SIZE_X):
@@ -86,6 +91,7 @@ class Minesweeper:
                     isMine = True
                     self.mines += 1
 
+                # 타일 구조체 정의
                 tile = {
                     "id": id,
                     "isMine": isMine,
@@ -97,7 +103,8 @@ class Minesweeper:
                     "button": Button(self.frame, image = gfx),
                     "mines": 0 # calculated after grid is built
                 }
-
+                
+                # 타일 클릭 이벤트
                 tile["button"].bind(BTN_CLICK, self.onClickWrapper(x, y))
                 tile["button"].bind(BTN_FLAG, self.onRightClickWrapper(x, y))
                 tile["button"].grid( row = x+1, column = y ) # offset by 1 row for timer
@@ -116,10 +123,12 @@ class Minesweeper:
         self.setup()
         self.refreshLabels()
 
+    #지뢰와 찾은 지뢰수 초기화
     def refreshLabels(self):
         self.labels["flags"].config(text = "Flags: "+str(self.flagCount))
         self.labels["mines"].config(text = "Mines: "+str(self.mines))
 
+    #게임 종료 시
     def gameOver(self, won):
         for x in range(0, SIZE_X):
             for y in range(0, SIZE_Y):
@@ -130,26 +139,31 @@ class Minesweeper:
 
         self.tk.update()
 
+        # won 변수로 이김과 짐에 따라 다른 메세지 출력
+
         msg = "You Win! Play again?" if won else "You Lose! Play again?"
         res = tkMessageBox.askyesno("Game Over", msg)
         if res:
-            self.restart()
+            self.restart() # 예 이면 다시 시작
+
         else:
-            self.tk.quit()
+            self.tk.quit() # 아니오면 게임 종료
 
     def updateTimer(self):
-        ts = "00:00:00"
+        ts = "00:00:00"  # 시 분 초 형식 맞추기
         if self.startTime != None:
-            delta = datetime.now() - self.startTime
-            ts = str(delta).split('.')[0] # drop ms
+            delta = datetime.now() - self.startTime # 현재시각 - 게임시작시각 = 게임 진행시각
+            ts = str(delta).split('.')[0] # drop ms/ 나노초 버리
             if delta.total_seconds() < 36000:
                 ts = "0" + ts # zero-pad
         self.labels["time"].config(text = ts)
         self.frame.after(100, self.updateTimer)
 
+    # 지뢰찾기의 주요 핵심
     def getNeighbors(self, x, y):
-        neighbors = []
-        coords = [
+        neighbors = []  # 빈 이웃리스트 생성
+
+        coords = [  # 자기 타일을 제외한 주변 타일 정의
             {"x": x-1,  "y": y-1},  #top right
             {"x": x-1,  "y": y},    #top middle
             {"x": x-1,  "y": y+1},  #top left
@@ -161,27 +175,32 @@ class Minesweeper:
         ]
         for n in coords:
             try:
-                neighbors.append(self.tiles[n["x"]][n["y"]])
+                neighbors.append(self.tiles[n["x"]][n["y"]])  # 예외처리를 통해 이웃 리스트에 추가
             except KeyError:
                 pass
         return neighbors
 
+    #좌클릭
     def onClickWrapper(self, x, y):
         return lambda Button: self.onClick(self.tiles[x][y])
-
+    
+    #우클릭
     def onRightClickWrapper(self, x, y):
         return lambda Button: self.onRightClick(self.tiles[x][y])
 
+    # 버튼 누르는 순간 시간 카운트 시작
     def onClick(self, tile):
         if self.startTime == None:
             self.startTime = datetime.now()
 
+        # 타일이 지뢰면 끝남
         if tile["isMine"] == True:
             # end game
             self.gameOver(False)
             return
 
         # change image
+        # 게임 다시 만들기
         if tile["mines"] == 0:
             tile["button"].config(image = self.images["clicked"])
             self.clearSurroundingTiles(tile["id"])
@@ -200,10 +219,10 @@ class Minesweeper:
 
         # if not clicked
         if tile["state"] == STATE_DEFAULT:
-            tile["button"].config(image = self.images["flag"])
-            tile["state"] = STATE_FLAGGED
+            tile["button"].config(image = self.images["flag"]) #지뢰로 이미지 변경
+            tile["state"] = STATE_FLAGGED #타일 상태 지뢰로 바꿈
             tile["button"].unbind(BTN_CLICK)
-            # if a mine
+            # if a mine / 지뢰일
             if tile["isMine"] == True:
                 self.correctFlagCount += 1
             self.flagCount += 1
@@ -236,10 +255,10 @@ class Minesweeper:
             return
 
         if tile["mines"] == 0:
-            tile["button"].config(image = self.images["clicked"])
+            tile["button"].config(image = self.images["clicked"]) #클릭 이미지로 타일 변경
             queue.append(tile["id"])
         else:
-            tile["button"].config(image = self.images["numbers"][tile["mines"]-1])
+            tile["button"].config(image = self.images["numbers"][tile["mines"]-1])# 지뢰가 아니면 숫자 이미지로 변
 
         tile["state"] = STATE_CLICKED
         self.clickedCount += 1
@@ -258,3 +277,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
